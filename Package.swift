93063d2b3b02bf287e1faefb22c8f8704b009cd0
gnejfejf2 <gnejfejf3@naver.com>
2023-07-25 15:12:55 +0900

    checkSum 까지 변경 진행

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NMapsMap",
-            url: "https://github.com/jaemyeong/NMapsMap-SPM/releases/download/3.16.0/NMapsMap.xcframework.zip",
+            url: "https://github.com/gnejfejf2/NaverMap/releases/download/3.16.0/NMapsMap.xcframework.zip",
             checksum: "efa36d05b32ec32eef6e26047d3830ace3906cd07e62a848ca9db59f02f0a2e4"
         ),
     ]
